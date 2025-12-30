d6be8943670c24ddf183100a666cd34354995bd9
jeancmr <jean.madiedo08@gmail.com>
2025-12-29 21:40:09 -0500

    chore: add images to assets

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+export const LoginPage = () => {
+  return (
+    <section
+      className="min-h-screen bg-cover"
+      style={{ backgroundImage: "url('/assets/background.webp')" }}
+    >
+      <h2>Login Page</h2>
+    </section>
+  );
+};
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router';
+import { LoginPage } from '../pages/LoginPage';
 
 const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<h2>Login Page</h2>} />
+        <Route path="login" element={<LoginPage />} />
       </Routes>
     </>
   );
